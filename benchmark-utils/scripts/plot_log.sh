38f993c17975b7d3ce264cc17c6d6065b77061fa
Sára Jobranová <sara.jobranova@gmail.com>
2024-03-24 18:24:03 +0100

    Updated plot script

--- benchmark-utils/scripts/plot_log.sh
+++ benchmark-utils/scripts/plot_log.sh
@@ -1,109 +1,108 @@
 #!/bin/bash
 
-IN_FILE=../results/measure-timeout.out
-OUT_FILE=../benchmarkMF.pdf
-OUT_FILE_ZOOM=../benchmarkMF_z.pdf
+# It is assumed that the script is run from the repository's home folder.
+# Expects path to csv file as first argument and the output file name (will be in pdf format) as second argument.
 
-MAX_TIME=100000
+# ===============================================
 
-RED=$(tput setaf 1)
-GREEN=$(tput setaf 2)
-YELLOW=$(tput setaf 3)
-NC=$(tput sgr0)
+BENCHMARKS_PATH="./benchmarks/no-measure/"
+TEMP_FILE_SUFFIX="-temp.out"
 
-awk -v maxt="$MAX_TIME" \
-        '/^BernsteinVazirani/ { if (match($2, /[0-9]*\.[0-9]*/)) { $2=substr($2, RSTART, RLENGTH); my_fail=0;} else { $2=maxt; my_fail=1;}; \
-                                if (match($3, /[0-9]*\.[0-9]*/)) { $3=substr($3, RSTART, RLENGTH); sliq_fail=0;} else { $3=maxt; sliq_fail=1; }; \
-                                print $3, $2}' $IN_FILE > bv-temp.out
+MIN_TIME=0.002
+MAX_TIME=3600
+ZOOM_MAX_TIME=1
 
-awk -v maxt="$MAX_TIME" \
-        '/^Feynman/ { if (match($2, /[0-9]*\.[0-9]*/)) { $2=substr($2, RSTART, RLENGTH); my_fail=0;} else { $2=maxt; my_fail=1;}; \
-                                if (match($3, /[0-9]*\.[0-9]*/)) { $3=substr($3, RSTART, RLENGTH); sliq_fail=0;} else { $3=maxt; sliq_fail=1; }; \
-                                print $3, $2}' $IN_FILE > feynman-temp.out
+MEDUSA_COLUMN=4
+OTHER_SIM_COLUMN=6
+OTHER_SIM_NAME="SliQSim"
+AXIS_LABEL="Runtime (s)"
 
-awk -v maxt="$MAX_TIME" \
-        '/^Grover/ { if (match($2, /[0-9]*\.[0-9]*/)) { $2=substr($2, RSTART, RLENGTH); my_fail=0;} else { $2=maxt; my_fail=1;}; \
-                                if (match($3, /[0-9]*\.[0-9]*/)) { $3=substr($3, RSTART, RLENGTH); sliq_fail=0;} else { $3=maxt; sliq_fail=1; }; \
-                                print $3, $2}' $IN_FILE > grover-temp.out
+# ===============================================
 
-awk -v maxt="$MAX_TIME" \
-        '/^MCToffoli/ { if (match($2, /[0-9]*\.[0-9]*/)) { $2=substr($2, RSTART, RLENGTH); my_fail=0;} else { $2=maxt; my_fail=1;}; \
-                                if (match($3, /[0-9]*\.[0-9]*/)) { $3=substr($3, RSTART, RLENGTH); sliq_fail=0;} else { $3=maxt; sliq_fail=1; }; \
-                                print $3, $2}' $IN_FILE > mctoffoli-temp.out
+parse_bench_dir() {
+    local temp_file_name=$dir_name$TEMP_FILE_SUFFIX
 
-awk -v maxt="$MAX_TIME" \
-        '/^MOGrover/ { if (match($2, /[0-9]*\.[0-9]*/)) { $2=substr($2, RSTART, RLENGTH); my_fail=0;} else { $2=maxt; my_fail=1;}; \
-                                if (match($3, /[0-9]*\.[0-9]*/)) { $3=substr($3, RSTART, RLENGTH); sliq_fail=0;} else { $3=maxt; sliq_fail=1; }; \
-                                print $3, $2}' $IN_FILE > mogrover-temp.out
+    awk -v maxt="$MAX_TIME" -v dir_name="$dir_name" \
+        -v medusa_col="$MEDUSA_COLUMN" -v other_sim_col="$OTHER_SIM_COLUMN" \
+        -F ',' \
+    'index($1, dir_name) == 1 {
+        if (!match($medusa_col, /[0-9]*\.?[0-9]+/)) {
+            $medusa_col=maxt;
+        };
+        if (!match($other_sim_col, /[0-9]*\.?[0-9]+/)) {
+            $other_sim_col=maxt;
+        };
+        print $other_sim_col, $medusa_col
+    }' "$in_file_path" > "$temp_file_name"
+}
 
-awk -v maxt="$MAX_TIME" \
-        '/^Random/ { if (match($2, /[0-9]*\.[0-9]*/)) { $2=substr($2, RSTART, RLENGTH); my_fail=0;} else { $2=maxt; my_fail=1;}; \
-                                if (match($3, /[0-9]*\.[0-9]*/)) { $3=substr($3, RSTART, RLENGTH); sliq_fail=0;} else { $3=maxt; sliq_fail=1; }; \
-                                print $3, $2}' $IN_FILE > random-temp.out
+plot() {
+    local out_file=$1
+    local min=$2
+    local max=$3
 
-awk -v maxt="$MAX_TIME" \
-        '/^RevLib/ { if (match($2, /[0-9]*\.[0-9]*/)) { $2=substr($2, RSTART, RLENGTH); my_fail=0;} else { $2=maxt; my_fail=1;}; \
-                                if (match($3, /[0-9]*\.[0-9]*/)) { $3=substr($3, RSTART, RLENGTH); sliq_fail=0;} else { $3=maxt; sliq_fail=1; }; \
-                                print $3, $2}' $IN_FILE > revlib-temp.out
+    gnuplot -persist <<-EOFMarker
+        set terminal pdf size 30cm,20cm enhanced background rgb 'white'
+        set output "$out_file"
+        
+        set multiplot
 
-gnuplot -persist <<-EOFMarker
-    set terminal pdf size 30cm,20cm enhanced background rgb 'white'
-    set output "$OUT_FILE"
-    
-    set multiplot
+        set origin 0,0
+        set size 1,1
+        set xlabel "$OTHER_SIM_NAME $AXIS_LABEL" font ",20"
+        set ylabel "MEDUSA $AXIS_LABEL" font ",20"
+        set logscale
+        set grid
 
-    set origin 0,0
-    set size 1,1
-    set xlabel "SliQSim Runtime (s)" font ",20"
-    set ylabel "MEDUSA Runtime (s)" font ",20"
-    set logscale
-    set grid
+        set xrange ["$min":"$max"]
+        set yrange ["$min":"$max"]
+        set size square
 
-    set xrange [0.002:"$MAX_TIME"]
-    set yrange [0.002:"$MAX_TIME"]
-    set size square
+        set xtics font ",20"
+        set ytics font ",20"
+        set key left top font ",20" box
 
-    set xtics font ",20"
-    set ytics font ",20"
-    set key left top font ",20" box
-    plot "bv-temp.out" title "BV" pt 6 lc 9 ps 1.5,\
-         "feynman-temp.out" title "Feynman" pt 7 lc 4 ps 1.5,\
-         "grover-temp.out" title "Grover" pt 5 lc 3 ps 1.5,\
-         "mctoffoli-temp.out" title "MCToffoli" pt 4 lc 8 ps 1.5,\
-         "mogrover-temp.out" title "MOGrover" pt 3 lc 9 ps 1.5,\
-         "random-temp.out" title "Random" pt 2 lc 2 ps 1.5,\
-         "revlib-temp.out" title "RevLib" pt 10 lc 6 ps 1.5,\
-         x lc 7 title ""
+        files = system("ls *$TEMP_FILE_SUFFIX")
+        pt_types = "6 7 5 4 3 11 1 2 10"
+        lc_colors = "9 4 3 8 9 5 8 2 6"
+        plot_files = ''
+        idx = 1
+        do for [file in files] {
+            title = system("echo " . file . " | sed 's/$TEMP_FILE_SUFFIX//'")
+            pt = word(pt_types, idx)
+            lc = word(lc_colors, idx)
+            plot_files = plot_files . '"' . file . '" title "' . title . '" pt ' . pt . ' lc ' . lc . ' ps 1.5,'
+            idx = idx + 1
+        }
+        # Remove trailing comma
+        plot_files = substr(plot_files, 1, strlen(plot_files)-1)
+        eval("plot " . plot_files)
+
+        # Plot the diagonal
+        plot x lc 7 title ""
 EOFMarker
+}
 
-gnuplot -persist <<-EOFMarker
-    set terminal pdf size 30cm,20cm enhanced background rgb 'white'
-    set output "$OUT_FILE_ZOOM"
-    
-    set multiplot
+# ===============================================
 
-    set origin 0,0
-    set size 1,1
-    set xlabel "SliQSim Runtime (s)" font ",20"
-    set ylabel "MEDUSA Runtime (s)" font ",20"
-    set logscale
-    set grid
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <results-file-path> <output-file-name>"
+    exit 1
+fi
 
-    set xrange [0.002:1]
-    set yrange [0.002:1]
-    set size square
+in_file_path=$1
+output_file_name=$2
 
-    set xtics font ",20"
-    set ytics font ",20"
-    set key left top font ",20" box
-    plot "bv-temp.out" title "BV" pt 6 lc 9 ps 1.5,\
-         "feynman-temp.out" title "Feynman" pt 7 lc 4 ps 1.5,\
-         "grover-temp.out" title "Grover" pt 5 lc 3 ps 1.5,\
-         "mctoffoli-temp.out" title "MCToffoli" pt 4 lc 8 ps 1.5,\
-         "mogrover-temp.out" title "MOGrover" pt 3 lc 9 ps 1.5,\
-         "random-temp.out" title "Random" pt 2 lc 2 ps 1.5,\
-         "revlib-temp.out" title "RevLib" pt 10 lc 6 ps 1.5,\
-         x lc 7 title ""
-EOFMarker
+output_file_extension="${output_file_name##*.}"
+zoom_output_file_name="${output_file_name%.*}_z.$output_file_extension"
 
-rm -f *-temp.out
+for dir in "$BENCHMARKS_PATH"*/; do
+    dir_name=$(basename "$dir")
+    dir_name=${dir_name#LP-}
+    parse_bench_dir
+done
+
+plot $output_file_name $MIN_TIME $MAX_TIME
+plot $zoom_output_file_name $MIN_TIME $ZOOM_MAX_TIME
+
+rm -f *"$TEMP_FILE_SUFFIX"
