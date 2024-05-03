c5a86b3f7c2466fc651502f5f402133532622de3
Sára Jobranová <sara.jobranova@gmail.com>
2024-05-03 20:21:32 +0200

    Updated benchmark plot script

--- benchmark-utils/scripts/plot.py
+++ benchmark-utils/scripts/plot.py
@@ -2,6 +2,14 @@ import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def map_tool_names(tool):
+    if tool == 'MEDUSA symbolic':
+        return "TOOL$_{loop}$"
+    elif tool == 'MEDUSA':
+        return "TOOL$_{base}$"
+    else:
+        return tool
+
 def graph_plot(xdata, ydata, title, xlabel, ylabel, filename):
     colors = plt.cm.get_cmap('tab10').colors
 
@@ -16,9 +24,11 @@ def graph_plot(xdata, ydata, title, xlabel, ylabel, filename):
             legend_labels.append(col.rsplit(' ', 1)[0])
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
-    plt.title(title)
+    #plt.title(title)
     plt.xticks(rotation=45, ha='right')
-    plt.legend(legend_handles, legend_labels, loc='upper left', bbox_to_anchor=(0, 1))
+    new_legend_labels = [map_tool_names(tool) for tool in legend_labels]
+    plt.legend(legend_handles, new_legend_labels, loc='upper left', bbox_to_anchor=(0, 1))
+    # comment for QC - shows every number
     plt.gca().xaxis.set_major_locator(plt.MultipleLocator(5))  # Show every 5th label
     plt.savefig(filename)
 
@@ -26,13 +36,13 @@ def plot_benchmark_data(csv_file, benchmark_name):
     df = pd.read_csv(csv_file)
 
     benchmark_data = df[df['Circuit'].str.startswith(benchmark_name+'/')]
-    circuit_names = [test.split('/')[1] for test in benchmark_data['Circuit']]
+    circuit_qubit_counts = benchmark_data['Qubits']
 
-    runtime_data = benchmark_data.iloc[:, 1::2]  # Select every other column starting from index 1
-    memory_usage_data = benchmark_data.iloc[:, 2::2]  # Select every other column starting from index 2
+    runtime_data = benchmark_data.iloc[:, 3::2]  # Select every other column starting from index 3
+    memory_usage_data = benchmark_data.iloc[:, 4::2]  # Select every other column starting from index 4
 
-    graph_plot(circuit_names, runtime_data, 'Runtime for ' + benchmark_name, 'Circuit Name', 'Runtime [s]', benchmark_name + "-runtime.pdf")
-    graph_plot(circuit_names, memory_usage_data, 'Memory Usage for ' + benchmark_name, 'Circuit Name', 'Memory Usage [MB]', benchmark_name + "-memory.pdf")
+    graph_plot(circuit_qubit_counts, runtime_data, 'Runtime for ' + benchmark_name, 'Number of Qubits', 'Runtime [s]', benchmark_name + "-runtime.pdf")
+    graph_plot(circuit_qubit_counts, memory_usage_data, 'Memory Usage for ' + benchmark_name, 'Number of Qubits', 'Memory Usage [MB]', benchmark_name + "-memory.pdf")
 
 
 if __name__ == "__main__":
